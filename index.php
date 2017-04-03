882032e829d8480c5947b78a1e0d6ba2489a8a03
Antoine <sotty.antoine@gmail.com>
2017-04-03 11:55:45 +0200

    Add CSS folder + .sass files

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>OSCCOP</title>
+    <link rel="stylesheet" href="css/conv_css/style.css">
   </head>
   <body>
-    <p>Bonjour</p>
-    <p>Ceci est un test pour le merge, ouaish tavu OKLM sans pression</p>
+    <div>
+      <p>Bonjour</p>
+      <p>Ceci est un test pour le merge, ouaish tavu OKLM sans pression</p>
+    </div>
   </body>
 </html>
