66847903ba075e97e91fba2e47eec397f01e86a2
Anelos <civodul.teros@gmail.com>
2017-04-06 13:02:24 +0200

    Test ajout de jeu v2

--- php/gamesearch.php
+++ php/gamesearch.php
@@ -1,20 +1,32 @@
 <?php
 include_once 'connexion.php';
-
+function recursivelyConvertToUTF8($data, $from = 'ISO-8859-1')
+{
+    if (!is_array($data)) {
+        return iconv($from, 'UTF-8', $data);
+    }
+    return array_map(function ($value) use ($from) {
+        return recursivelyConvertToUTF8($value, $from);
+    }, $data);
+}
 if (isset($_POST['game']) && isset($_POST['typeOfSearch'])) {
     $i = 0;
     $gameData = array();
-    $game = $_POST['game'];
+    $input = $_POST['game'];
     if ($_POST['typeOfSearch'] == 'console') {
-        $req = mysqli_query($cnx, "SELECT jeu.id_jeu, jeu.nom_jeu, console.id_console,console.nom_console FROM console INNER JOIN jeu ON console.id_console=jeu.id_console WHERE console.nom_console LIKE '%$game%'");
+        $qry = "SELECT jeu.id_jeu, jeu.nom_jeu, console.id_console,console.nom_console FROM console INNER JOIN jeu ON console.id_console=jeu.id_console WHERE console.nom_console LIKE '%$input%'";
     } else {
-        $req = mysqli_query($cnx, "SELECT jeu.id_jeu, jeu.nom_jeu, console.id_console,console.nom_console FROM jeu INNER JOIN console ON jeu.id_console=console.id_console WHERE jeu.nom_jeu LIKE '%$game%'");
+        $qry = "SELECT jeu.id_jeu, jeu.nom_jeu, console.id_console,console.nom_console FROM jeu INNER JOIN console ON jeu.id_console=console.id_console WHERE jeu.nom_jeu LIKE '%$input%'";
     }
-    while ($data = mysqli_fetch_assoc($req)) {
-        $gameData[$i] = $data;
-        $i++;
+    $req = $cnx->prepare($qry);
+    $req->execute();
+
+    while ($data = $req->fetch(PDO::FETCH_ASSOC)) {
+        $gameData[] = $data;
     }
 
-    echo json_encode($gameData);
-    mysqli_close($cnx);
+    $dataJson = json_encode(recursivelyConvertToUTF8($gameData));
+    echo $dataJson;
+    $req->closeCursor();
 }
+$cnx = null;
