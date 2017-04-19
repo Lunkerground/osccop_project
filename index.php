d6807f7a15d909a9182630ea7729c718c476b16d
Antoine <sotty.antoine@gmail.com>
2017-04-19 09:38:37 +0200

    Start index.php design

--- index.php
+++ index.php
@@ -17,15 +17,95 @@
 
     <header>
 
+      <!-- Banner -->
       <div class="banner">
 
       </div>
 
+      <!-- Navbar -->
       <?php
 
         include('pages/components/navbar.php');
 
       ?>
+
+      <div class="container-fluid">
+
+        <!-- Important Informations - Next Dates + Football Games Expo -->
+        <div class="row main_important">
+
+          <!-- Next Events to come -->
+          <div class="col-xs-12 col-sm-6 next_events">
+            <div class="row">
+
+              <div class="col-xs-12">
+                <h1 class="text-left">Évènements à venir</h1>
+              </div>
+
+              <!-- First Event to come -->
+              <div class="col-xs-12 col-sm-6" style="padding: 15px;">
+                <div style="background-color: grey; height: 200px; width: 50%; float: left;">
+
+                </div>
+                <div style="background-color: white; height: 200px; width: 50%; float: left;" class="text-center">
+                  <h3>Title</h3>
+                  <h3>Date</h3>
+                  <h3><a href="#">+ d'infos</a></h3>
+                </div>
+              </div>
+
+              <!-- Second Event to come -->
+              <div class="col-xs-12 col-sm-6" style="padding: 15px;">
+                <div style="background-color: grey; height: 200px; width: 50%; float: left;">
+
+                </div>
+                <div style="background-color: white; height: 200px; width: 50%; float: left;" class="text-center">
+                  <h3>Title</h3>
+                  <h3>Date</h3>
+                  <h3><a href="#">+ d'infos</a></h3>
+                </div>
+              </div>
+
+              <div class="col-xs-12">
+                <button type="button" name="button">Voir les anciens évènements</button>
+              </div>
+
+            </div>
+          </div>
+
+          <!-- Football Exposition -->
+          <div class="col-xs-12 col-sm-6 expo_foot">
+            <div class="row">
+
+              <div class="col-xs-12">
+                <h1 class="text-right">Exposition Foot</h1>
+              </div>
+
+              <!-- Expo Text -->
+              <div class="col-xs-12 col-sm-6" style="padding: 15px;">
+                <div style="background-color: white; height: 200px">
+
+                </div>
+              </div>
+
+              <!-- Expo Img -->
+              <div class="col-xs-12 col-sm-6" style="padding: 15px;">
+                <div style="background-color: white; height: 200px">
+
+                </div>
+              </div>
+
+              <div class="col-xs-12">
+                <button type="button" name="button">Plus d'informations</button>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
 
     </header>
 
