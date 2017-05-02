81e06bc5b9411d3d0fc7ee0202d8de37f60833dc
Antoine <sotty.antoine@gmail.com>
2017-05-02 11:11:34 +0200

    Welcome page almost done

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
     <!-- Bootstrap - Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+    <!-- Font-Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <!-- CSS -->
     <link rel="stylesheet" href="css/convert/style.css">
 
@@ -23,11 +26,7 @@
       </div>
 
       <!-- Navbar -->
-      <?php
-
-        include('pages/components/navbar.php');
-
-      ?>
+      <?php include('pages/components/_navbar.php'); ?>
 
       <div class="container-fluid">
 
@@ -45,26 +44,32 @@
 
               <!-- First Event to come -->
               <div class="col-xs-12 col-sm-6">
+
                 <div class="medias_container">
                   <img src="images/test_affiche.jpg" alt="affiche">
                 </div>
+
                 <div class="text_container text-center">
                   <p>Les 20 ans de la Playstation</p>
                   <p>Date</p>
                   <p><a href="#" class="blue third_links">+ d'infos</a></p>
                 </div>
+
               </div>
 
               <!-- Second Event to come -->
               <div class="col-xs-12 col-sm-6">
+
                 <div class="medias_container">
                   <img src="images/test_affiche.jpg" alt="affiche">
                 </div>
+
                 <div class="text_container text-center">
                   <p>Title</p>
                   <p>Date</p>
                   <p><a href="#" class="blue third_links">+ d'infos</a></p>
                 </div>
+
               </div>
 
               <!-- Section Button -->
@@ -86,16 +91,20 @@
 
               <!-- Expo Text -->
               <div class="col-xs-12 col-sm-6">
+
                 <div class="text_container">
                   <p class="text-justify">Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium.</p>
                 </div>
+
               </div>
 
               <!-- Expo Img -->
               <div class="col-xs-12 col-sm-6">
+
                 <div class="medias_container">
                   <img src="images/expo_foot.jpg" alt="expo foot">
                 </div>
+
               </div>
 
               <!-- Section Button -->
@@ -109,66 +118,148 @@
 
         </div> <!-- End of .main_important -->
 
-        <!-- Sections' Presentation -->
-        <div class="row section_presentation">
-
-          <!-- Media Section -->
-          <div class="col-xs-12 col-sm-6 media_section">
-            <div class="title_container">
-              <h3>Médias</h3>
-            </div>
-            <div class="content_container">
-
-            </div>
-          </div>
-
-          <!-- Feedbacks + Random Section -->
-          <div class="col-xs-12 col-sm-6 feed_random">
-            <div class="row">
-
-              <!-- Feedbacks -->
-              <div class="col-xs-12 feedback_section">
-                <div class="title_container">
-                  <h3>Compte-rendus</h3>
-                </div>
-                <div class="content_container">
-
-                </div>
-              </div>
-
-              <!-- Random -->
-              <div class="col-xs-12 random_section">
-                <div class="title_container">
-                  <h3>Le coin Random</h3>
-                </div>
-                <div class="content_container">
-                  <div class="first_random" style="display: flex; padding: 25px">
-                    <div class="medias_container" style="width: 40%">
-                      <img src="images/dbzhd_snes.jpg" alt="">
-                    </div>
-                    <div class="text_container" style="width: 60%; margin-left: 25px">
-                      <p class="text-justify">Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium.</p>
-                    </div>
-                  </div>
-                  <div class="second_random" style="display: flex; padding: 25px">
-                    <div class="medias_container" style="width: 40%">
-                      <img src="images/gamecube.jpg" alt="">
-                    </div>
-                    <div class="text_container" style="width: 60%; margin-left: 25px">
-                      <p class="text-justify">Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-            </div>
-          </div>
-
-        </div>
-
       </div> <!-- End of .container-fluid -->
 
-    </header>
+    </header> <!-- End of header section -->
+
+    <div class="container-fluid">
+
+      <!-- Sections' Presentation -->
+      <div class="row section_presentation">
+
+        <!-- Media Section -->
+        <div class="col-xs-12 col-sm-6 media_section">
+
+          <div class="title_container">
+            <h3>Médias</h3>
+          </div>
+
+          <div class="content_container">
+
+            <div class="tests_videos_container">
+
+              <div class="medias_container">
+                <img src="images/dbzhd_snes.jpg" alt="" class="img-responsive">
+              </div>
+
+              <div class="text_container">
+                <div class="title_container">
+                  <h3>Title</h3>
+                </div>
+                <p class="text-justify">Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium.</p>
+              </div>
+
+            </div> <!-- End of .tests_videos_container -->
+
+            <div class="podcasts_container">
+
+              <div class="medias_container">
+                <img src="images/dbzhd_snes.jpg" alt="" class="img-responsive">
+              </div>
+
+              <div class="text_container">
+                <div class="title_container">
+                  <h3>Title</h3>
+                </div>
+                <p class="text-justify">Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium.</p>
+              </div>
+
+            </div> <!-- End of .podcasts_container -->
+
+            <div class="tests_written_container">
+
+              <div class="medias_container">
+                <img src="images/dbzhd_snes.jpg" alt="" class="img-responsive">
+              </div>
+
+              <div class="text_container">
+                <div class="title_container">
+                  <h3>Title</h3>
+                </div>
+                <p class="text-justify">Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium.</p>
+              </div>
+
+            </div> <!-- End of .tests_written_container -->
+
+          </div> <!-- End of .content_container -->
+
+        </div> <!-- End of .media_section -->
+
+        <!-- Feedbacks + Random Section -->
+        <div class="col-xs-12 col-sm-6 feed_random">
+          <div class="row">
+
+            <!-- Feedbacks -->
+            <div class="col-xs-12 feedback_section">
+
+              <div class="title_container">
+                <h3>Compte-rendus</h3>
+              </div>
+
+              <div class="content_container">
+
+                <div class="medias_container">
+                  <img src="images/expo_foot.jpg" alt="" class="img-responsive">
+                  <img src="images/expo_foot.jpg" alt="" class="img-responsive">
+                </div>
+
+                <div class="text_container">
+                  <h3 class="text-justify">Titre <small>00/00/00</small></h3>
+                  <p class="text-justify">Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium.</p>
+                </div>
+
+              </div>
+            </div>
+
+            <!-- Random -->
+            <div class="col-xs-12 random_section">
+
+              <div class="title_container">
+                <h3>Le coin Random</h3>
+              </div>
+
+              <div class="content_container">
+
+                <div class="first_random">
+
+                  <div class="medias_container">
+                    <img src="images/dbzhd_snes.jpg" alt="" class="img-responsive">
+                  </div>
+
+                  <div class="text_container">
+                    <p class="text-justify">Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium.</p>
+                  </div>
+
+                </div> <!-- End of .first_random -->
+
+                <div class="second_random">
+
+                  <div class="medias_container">
+                    <img src="images/gamecube.jpg" alt="" class="img-responsive">
+                  </div>
+
+                  <div class="text_container">
+                    <p class="text-justify">Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium.</p>
+                  </div>
+
+                </div> <!-- End of .second_random -->
+
+              </div> <!-- End of .content_container -->
+
+            </div> <!-- End of .random_section -->
+
+          </div>
+        </div> <!-- End of .feed_random -->
+
+      </div> <!-- End of .section_presentation -->
+
+    </div> <!-- End of .container-fluid -->
+
+    <footer>
+
+      <?php include 'pages/components/_footer.php' ?>
+
+    </footer>
 
     <!-- Jquery -->
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
