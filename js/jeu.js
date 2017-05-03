a406d30d248655b0cef6a88379beabb4cda91207
Anelos <civodul.teros@gmail.com>
2017-05-03 14:48:26 +0200

    ajout event finit dans sa globalité

--- js/jeu.js
+++ js/jeu.js
@@ -1,10 +1,11 @@
 var NBGAMEMAXTODISPLAY = 13
 var gameData
 var queryOffset = 0
+
 $(document).ready(function () {
-  let formContent = {}
   let games = []
   let idGame = []
+
   request('') // requete pour afficher tout les jeux par défaut
 
   // il se passe des trucs si je commence à remplir le champ de recherche de jeu...
@@ -18,33 +19,47 @@ $(document).ready(function () {
   $('#addEvent').click(function () {
     $('#eventModal').modal('show')
 
-    // Je récupère toutes les données des tags "input" du formulaire
-    $('#addEventForm input').each(function () {
-      addToContent(
-        formContent,
-        $(this).attr('name'),
-        $(this).val()
-      )
-    })
-    // Je récuprèe les données du textarea du formulaire
-    addToContent(
-      formContent,
-      $('#addEventForm textarea').attr('name'),
-      $('#addEventForm textarea').val()
-    )
+    // Je récupère toutes les données du formulaire
+    let myform = document.getElementById('addEventForm')
+    let formContent = new FormData(myform)
 
     // Je récupère la liste des jeux sélectionnés et leurs id
     $('.choosedgame .gameName').each(function () {
       games.push('<p>' + $(this).html() + '</p>')
       idGame.push($(this).parents('.game').attr('id').substr(1))
     })
+    // que j'ajoute aux les données du formulaire
+    formContent.append('games', idGame)
 
     // je rentre toute les données précédemment acquise dans le modal pour validation
-    addContentToModal(formContent.eventName, formContent.eventDate, formContent.eventPresentation, games)
+    addContentToModal(
+      formContent.get('eventName'),
+      formContent.get('eventDate'),
+      formContent.get('eventPresentation'),
+      games
+    )
+    console.log(formContent.get('eventPoster'))
+    $('#validate').click(function () {
+      $.ajax({
+        url: '../php/ajoutEvent.php',
+        data: formContent,
+        type: 'POST',
+        contentType: false,
+        processData: false,
+        success: () => {
+          $('#eventModal').modal('hide')
+          $("#addEventForm")[0].reset()
+          $('.choosedgame').html("")
+          idGame = []
+          games = []
+          alertMessage('SUCCESS', 'Ce nouvel événement a été correctement ajouté à la base de donnée')
+        }
+      })
+    })
   })
 })
 
-// Fonction de requête pour les jeux
+// Fonction de requête pour la recherche de jeux
 var request = (inpVal) => {
   $.ajax({
     method: 'GET',
@@ -63,7 +78,7 @@ var request = (inpVal) => {
       if (numberOfGameFound > NBGAMEMAXTODISPLAY) {
         $('#pages').html('')
         for (let p = 1; p <= Math.ceil(numberOfGameFound / NBGAMEMAXTODISPLAY); p++) {
-          $('#pages').append("<a class='pagenb'>" + p + ' </a>')
+          $('.pagination').append("<li><a class='pagenb'>" + p + ' </a></li>')
         }
         $('a.pagenb').click(function () {
           queryOffset = $('.pagenb').index($(this)) * NBGAMEMAXTODISPLAY
@@ -77,12 +92,6 @@ var request = (inpVal) => {
         })
       } else {
         $('#pages').html('')
-        resultDisplay(
-          gameData,
-          queryOffset,
-          NBGAMEMAXTODISPLAY,
-          numberOfGameFound
-        )
       }
       // affiche les jeux correspondant à la recherche
       resultDisplay(
@@ -91,12 +100,11 @@ var request = (inpVal) => {
         NBGAMEMAXTODISPLAY,
         numberOfGameFound
       )
-    },
-    error: function (request, status, error) {
-      console.log(error)
     }
   })
 }
+
+// fonction pour afficher la liste des jeux de la recherche
 var resultDisplay = (data, offset, limit, gameCount) => {
   $('#gamelist').html('')
   $('#nbGame').html('')
@@ -113,13 +121,12 @@ var resultDisplay = (data, offset, limit, gameCount) => {
   }
   // affiche les jeux
   for (let i = offset; i <= (offset + limitOfGame); i++) {
-    $('#gamelist').append('<div class="game row" id="G' + data[i].id_jeu + '"><div class="col-lg-10">' + data[i].nom_jeu + ' <small>' + data[i].nom_console + '</small></div><div class="col-lg-2"><a class="selectGame">+</a></div></div>')
+    $('#gamelist').append('<div class="game row" id="G' + data[i].id_jeu + '"><div class="col-lg-10">' + data[i].nom_jeu + ' <small>' + data[i].nom_console + '</small></div><div class="col-lg-2"><a class="selectGame"><span class="glyphicon glyphicon-plus"></span></a></div></div>')
   }
   // Ajout de jeu dans la selection
   $('.selectGame').click(function () {
     let gIndex = $('.game').index($(this).parents().parents())
     let gId = $(this).parents().parents().attr('id').substr(1)
-    console.log(gId);
     // vérfie si le jeu fait déjà parti de la sélection
     let alreadySelected = false
     $('.choosedgame .game').each(function () {
@@ -157,20 +164,19 @@ var readURL = (input) => {
 
 // fonction pour l'affichage des jeux qui ont été sélectionnés
 var selectedDisplay = (gameId, gameName, consoleName) => {
-  $('.choosedgame').append('<div class="game row" id="C' + gameId + '"><div class="col-lg-10 gameName">' + gameName + ' <small>' + consoleName + '</small></div><div class="col-lg-2"> <a class="removeGame">X</a></div></div>')
-}
-
-// fonction pour le stockage des données du formulaire
-var addToContent = (object, key, value) => {
-  if (key !== undefined) {
-    object[key] = value
-  }
+  $('.choosedgame').append('<div class="game row" id="C' + gameId + '"><div class="col-lg-10 gameName">' + gameName + ' <small>' + consoleName + '</small></div><div class="col-lg-2"> <a class="removeGame"><span class="glyphicon glyphicon-remove"></span></a></div></div>')
 }
 
 // fonctin de remplissage du modal de prévisualisation/validation
 var addContentToModal = (eventtitle, eventdate, eventtext, eventgames) => {
-  $('#modalEventTitle').append(eventtitle)
-  $('#modalEventDate').append(eventdate)
-  $('#modalEventText').append(eventtext)
-  $('#modalEventGames').append(eventgames)
+  $('#modalEventTitle').html(eventtitle)
+  $('#modalEventDate').html(eventdate)
+  $('#modalEventText').html(eventtext)
+  $('#modalEventGames').html(eventgames)
+}
+
+var alertMessage = (messageTitle, messageText) => {
+  $('#modalMessageTitle').html(messageTitle)
+  $('#MessageText').html(messageText)
+  $('#messageModal').modal('show')
 }
